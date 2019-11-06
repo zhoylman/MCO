ce06194236bf7571c620c3d61532868a94479606
Zach Hoylman <zhoylman@gmail.com>
2019-11-06 16:23:33 -0700

    test

--- MCO_data_requests/holden_11_6_19.R
+++ MCO_data_requests/holden_11_6_19.R
@@ -1,4 +1,4 @@
 library(curl)
 
-curl_download('https://mesonet.climate.umt.edu/api/observations?latest=false&start_time=2019-09-01&tz=US%2FMountain&wide=true&type=csv',
+curl_download('http://cfcmesonet-test.cfc.umt.edu/api/observations?latest=false&start_time=2019-10-06&type=csv',
               destfile = "/home/zhoylman/temp/mesonet_data.csv")
