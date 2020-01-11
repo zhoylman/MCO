3e230b0d9c58773da7833ec03db8ecefb8f2a99d
Zach Hoylman <zhoylman@gmail.com>
2020-01-10 21:04:52 -0700

    loop though stations

--- R/mesonet_plotting.R
+++ R/mesonet_plotting.R
@@ -34,7 +34,12 @@ simple_plotly = function(data,name_str,col,ylab){
     add_lines()
 }
 
+stations = unique(get_current$station_key)
+
 for(i in 1:length(names_str)){
+  data = get_current %>%
+    dplyr::filter(station_key == stations[i])
+  
   plots[[i]] = simple_plotly(data,names_str[i], col[i], ylab[i])
 }
 
@@ -62,4 +67,35 @@ final = subplot(plots[[1]], plots[[2]], plots[[3]], plots[[4]], vwc, nrows = 5, 
   layout(annotations = a)%>%
   layout(legend = list(x = 100, y = 0.1))
 
-htmlwidgets::saveWidget(final, "~/MCO/data/mesonet/test.html",  selfcontained = F)
+
+for(i in 1:10){
+  for(i in 1:length(names_str)){
+    plots[[i]] = simple_plotly(data,names_str[i], col[i], ylab[i])
+  }
+  
+  vwc = data %>%
+    dplyr::filter(name %like% "soilwc") %>%
+    plot_ly(x = ~datetime, y = ~value,  colors = "black", name = ~name, type = 'scatter', mode = 'lines', showlegend=T) %>%
+    layout(yaxis = list(
+      title = paste0("Volumetric Water Content\n(m3/m3)")))%>%
+    add_lines() 
+  
+  # annotations
+  a <- list(
+    text = "arskeogh",
+    xref = "paper",
+    yref = "paper",
+    yanchor = "bottom",
+    xanchor = "center",
+    align = "center",
+    x = 0.5,
+    y = 1,
+    showarrow = FALSE
+  )
+  
+  final = subplot(plots[[1]], plots[[2]], plots[[3]], plots[[4]], vwc, nrows = 5, shareX = T, titleY = T, titleX = T) %>%
+    layout(annotations = a)%>%
+    layout(legend = list(x = 100, y = 0.1))
+  
+  htmlwidgets::saveWidget(final, paste0("~/MCO/data/mesonet/",stations[i],"_test.html"), selfcontained = F, libdir = "~/MCO/data/mesonet/libs")
+}
